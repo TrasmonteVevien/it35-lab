b1a91afdd81f2acdc30fd655699543f48bcb4b90
vevien <vevienabiera@gmail.com>
2025-04-10 21:50:20 +0800

    Updates

--- assets/ios.transition-legacy-DTfJt82D.js
+++ assets/ios.transition-legacy-DTfJt82D.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DjibuHo2.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-D23ImpOa.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
