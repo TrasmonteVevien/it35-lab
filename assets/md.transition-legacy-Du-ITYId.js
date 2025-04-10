f4f75346d34f102ba60c37540e9108db40341c5f
vevien <vevienabiera@gmail.com>
2025-04-10 23:53:43 +0800

    Updates

--- assets/md.transition-legacy-Du-ITYId.js
+++ assets/md.transition-legacy-Du-ITYId.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BeNsJcyR.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-BG8R60BB.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
