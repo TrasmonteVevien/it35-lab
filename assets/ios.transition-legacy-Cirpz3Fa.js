db79f87bfeb5b4f401e7e7557832bdbc72e6c74a
vevien <vevienabiera@gmail.com>
2025-04-11 00:00:45 +0800

    Updates

--- assets/ios.transition-legacy-Cirpz3Fa.js
+++ assets/ios.transition-legacy-Cirpz3Fa.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BG8R60BB.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-BeNsJcyR.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
