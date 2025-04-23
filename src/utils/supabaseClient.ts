3b106ec0fee5308fc50422f8689f7994c3948c00
vevien <vevienabiera@gmail.com>
2025-04-23 18:05:39 +0800

    Added env file/Rerouting supabase

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://lbmptucbkfjnmsakgxbp.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxibXB0dWNia2Zqbm1zYWtneGJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI4NjAzNDMsImV4cCI6MjA1ODQzNjM0M30.qLIJBbK9-pujjkyPsR79G7EwV-ngRNIHlDKEwAAQedo';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
