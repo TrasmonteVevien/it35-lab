e1a695dc196c6a1048ca9cb6f484733a69f2bf15
vevien <vevienabiera@gmail.com>
2025-04-23 21:20:08 +0800

    Updates

--- assets/md.transition-legacy-C3PyS5Qe.js
+++ assets/md.transition-legacy-C3PyS5Qe.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CfvOUZvy.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-K54QYbTz.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
